782e6455d7573f87cb1d2767021bd05a1dcbe26e
Rudransh Singh Mahra <rs.mahra4546@gmail.com>
2021-08-20 11:59:17 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/flutter_swipe_delete_listview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_swipe_delete_listview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  final List<String> items = new List<String>.generate(30, (i) => "Items ${i+1}");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Swipe To Dismiss"),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context,int index){
+            return Dismissible(
+                key: Key(items[index]),
+              onDismissed: (direction){
+                  setState(() {
+                    items.removeAt(index);
+                  }); //setState is compulsory as to avoid RangeError , and
+                  Scaffold.of(context).showSnackBar(new SnackBar(
+                      content: Text("ITEM IS SUCCESSFULLY REMOVED")));
+              },
+              background: Container(
+                color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0),
+              ),
+              child: ListTile(
+                title: Text("${items[index]}"),
+              ),
+
+                );
+          }),
+    );
+  }
+}
